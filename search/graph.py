ee13862a5e4bedb419adc9b62269289f8ef03660
Alex Lee <alex.j.lee03@gmail.com>
2022-01-10 15:18:13 -0800

    implemented shortest path func

--- search/graph.py
+++ search/graph.py
@@ -17,88 +17,54 @@ class Graph:
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
         self.nodes = self.graph.nodes
 
-    def bfs(self, start: str, end: str = None):
+    def get_shortest_path(self, in_adj_map: dict, start: str, end: str):
+        """[summary]
+
+        Args:
+            in_adj_map (dict): [description]
+            start (str): [description]
+            end (str): [description]
+
+        Returns:
+            [type]: [description]
         """
-        SOLUTION: write a method that performs a breadth first traversal and pathfinding on graph G
 
-        * If there's no end node, just return a list with the order of traversal
-        * If there is an end node and a path exists, return a list of the shortest path
-        * If there is an end node and a path does not exist, return None
+        shortest_path = [[end]]
+        seen = set()
+        neighbors = in_adj_map[
+            end
+        ].copy()  # needed so we don't modify the original list in in_adj_map
 
-        """
+        while neighbors:
+            node = neighbors.pop(-1)
+            children = in_adj_map[node]
 
-        if start not in self.nodes:
-            raise ValueError(
-                f"Provided node name: {start} not a valid node in the graph found at {self.filename}"
-            )
-        if (end is not None) and (end not in self.nodes):
-            raise ValueError(
-                f"Provided node name: {end} not a valid node in the graph found at {self.filename}"
-            )
+            neighbors.extend(children)
 
-        depth = 1
-        seen = {start: 0}
+            if len(children) == 1:
+                shortest_path.append(children)
+            else:
+                shortest_path.append(children)
 
-        parent_dict = defaultdict(list)
-        finished = False
-        neighbors = sorted(self.graph.neighbors(start))
-
-        while finished is False:
-            queue = set()
-            for neighbor in neighbors:
-                if neighbor not in seen.keys():
-                    queue.add(neighbor)
-
-            if end is not None and end in queue:
+            if start in children:
                 break
 
-            queue = sorted(queue)
+        return list(reversed(shortest_path))
 
-            seen.update({node: depth for node in queue})
+    def bfs(self, start: str, end: str = None, return_reverse_adj: bool = False):
+        """[summary]
 
-            neighbors = []
+        Args:
+            start (str): [description]
+            end (str, optional): [description]. Defaults to None.
+            return_reverse_adj (bool, optional): [description]. Defaults to False.
 
-            for node in sorted(queue):
-                # print(node, node in self.nodes)
-                neighbors.extend(self.graph.neighbors(node))
+        Raises:
+            ValueError: [description]
+            ValueError: [description]
 
-            neighbors.sort()
-
-            if len(neighbors) == 0:
-                break  # end of graph traversal
-
-            depth += 1
-
-        return seen
-
-
-import networkx as nx
-from typing import Union
-
-
-class Graph:
-    """
-    Class to contain a graph and your bfs function
-    """
-
-    def __init__(self, filename: str):
-        """
-        Initialization of graph object which serves as a container for
-        methods to load data and
-
-        """
-        self.filename = filename
-        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
-        self.nodes = self.graph.nodes
-
-    def bfs(self, start: str, end: str = None):
-        """
-        SOLUTION: write a method that performs a breadth first traversal and pathfinding on graph G
-
-        * If there's no end node, just return a list with the order of traversal
-        * If there is an end node and a path exists, return a list of the shortest path
-        * If there is an end node and a path does not exist, return None
-
+        Returns:
+            [type]: [description]
         """
 
         if start not in self.nodes:
@@ -110,14 +76,47 @@ class Graph:
                 f"Provided node name: {end} not a valid node in the graph found at {self.filename}"
             )
 
-        depth = 0
-        seen = tuple(sorted(self.neighbors(start)))
+        seen = []
+
+        in_deg_adjacency = defaultdict(list)
+
+        neighbors = {start: []}
+        for neighbor in sorted(self.graph.neighbors(start)):
+            neighbors[start].append(neighbor)
+            in_deg_adjacency[neighbor].append(start)
+
         finished = False
 
-        for neighbor in neighbors:
-            if neighbor not in seen:
-                seen.append(neighbor)
+        while not finished:
+            queue = []
+            for top_node in neighbors.keys():
+                for top_node_neighbor in neighbors[top_node]:
+                    if top_node_neighbor not in seen:
+                        # print(top_node_neighbor)
+                        queue.append(top_node_neighbor)
 
-            depth += 1
+            seen.extend(queue)
 
-        return
+            neighbors = {}
+            for node in queue:
+                node_neighbors = sorted(self.graph.neighbors(node))
+                neighbors[node] = node_neighbors
+                for child_node in node_neighbors:
+
+                    if (child_node in seen) or (node in in_deg_adjacency[child_node]):
+                        continue
+
+                    in_deg_adjacency[child_node].append(node)
+
+            if len(neighbors) == 0:
+                break
+
+            if (end is not None) and (end in seen):
+                finished = True
+
+        if end is None:
+            return seen
+        elif (end is not None) and (finished is True):
+            return self.get_shortest_path(in_deg_adjacency, start, end)
+        else:
+            return None
